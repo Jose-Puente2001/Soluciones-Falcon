fb0a591ab0a8a70ed87b7dcf86a42dff78b0229b
Jose-Puente2001 <josegabrielpuente75@gmail.com>
2020-06-10 11:51:38 -0400

    first commit

---
+++ src/main.js
@@ -0,0 +1,6 @@
+ScrollReveal().reveal('.header');
+ScrollReveal().reveal('.service', {delay:500});
+ScrollReveal().reveal('.flex', {delay:500});
+ScrollReveal().reveal('.title', {delay:500});
+ScrollReveal().reveal('.title-img', {delay:500});
+ScrollReveal().reveal('.footer', {delay:500});
